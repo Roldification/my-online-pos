4060413a5d3693f359fdaddd84367790cd11e2f3
Harold <roldify27@gmail.com>
2022-04-25 20:58:43 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.appview')
+
+<div>
+    @section('title')
+        weh?
+    @endsection
+
+    @section('content')
+    <example-component>
+
+    </example-component>
+    @endsection
+    
+</div>
